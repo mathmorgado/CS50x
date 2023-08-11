1465539dfac92bdc4f1156254394cd2d68d6771e
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-08-11 12:00:44 -0300

    Create meow.c

---
+++ semana 1/exercise/meow.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 0;
+    while (i < 3)
+    {
+        printf("Meow.\n");
+        i++;
+    }
+
+    for (int cont = 0; cont < 3; cont++)
+    {
+        printf("Wuof\n");
+    }
+}
