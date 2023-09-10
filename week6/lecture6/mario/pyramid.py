918cd9fdaab637b2af5271d6fc9c04c3a15c536c
Matheus R. Morgado <94700017+mathmorgado@users.noreply.github.com>
2023-09-10 12:43:12 -0300

    Create pyramid.py

---
+++ week6/lecture6/mario/pyramid.py
@@ -0,0 +1,35 @@
+def pyramid(height):
+    for i in range(1, height + 1):
+        print("#" * i)
+
+
+def invert_pyramid(height):
+    for i in range(1, height + 1):
+        if i != height:
+            print(" " * ((height - 1) - i), "#" * i)
+        else:
+            print("#" * i)
+
+def double_pyramid(height):
+    for i in range(1, height + 1):
+        if i != height:
+            print(" " * ((height - 1) - i), "#" * i, end=" ")
+            print("#" * i)
+        else:
+            print("#" * i, end=" ")
+            print("#" * i)
+
+
+while True:
+    try:
+        height = int(input("Height: "))
+        if height > 0:
+            break
+    except ValueError:
+        print("Invalid value!\n")
+
+pyramid(height)
+print("")
+invert_pyramid(height)
+print("")
+double_pyramid(height)
